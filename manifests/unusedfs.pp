c9ab0eabbe2a9883d67da9a0db14ee434bf847a4
Ibn English <Ibn_robinson@hotmail.com>
2023-06-11 21:25:12 +1000

    updated summary

--- manifests/unusedfs.pp
+++ manifests/unusedfs.pp
@@ -1,7 +1,7 @@
 # @summary 
 #   completes CIS control 1.1.1.1 Ensure mounting of cramfs filesystems is disabled (Automated)
 #   completes CIS control 1.1.1.2 Ensure mounting of squashfs filesystems is disabled (Automated)
-#
+#   completes CIS control 1.1.1.3 Ensure mounting of udf filesystems is disabled (Automated) 
 class rockysoe::unusedfs {
   file { '/etc/modprobe.d':
     ensure => directory,
