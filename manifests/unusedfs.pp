55e0311eddff6009b8fa571127cb2a259956c350
Ibn English <Ibn_robinson@hotmail.com>
2023-06-11 20:38:38 +1000

    refactored to include if statements

--- manifests/unusedfs.pp
+++ manifests/unusedfs.pp
@@ -2,7 +2,10 @@
 #   completes CIS control 1.1.1.1 Ensure mounting of cramfs filesystems is disabled (Automated)
 #   completes CIS control 1.1.1.2 Ensure mounting of squashfs filesystems is disabled (Automated)
 #
-class rockysoe::unusedfs {
+class rockysoe::unusedfs (
+  $enforce_cramfs = true,
+  $enforce_squashfs = true,
+) {
   file { '/etc/modprobe.d':
     ensure => directory,
     owner  => 'root',
@@ -10,33 +13,37 @@ class rockysoe::unusedfs {
     mode   => '0755',
   }
 
-  file { '/etc/modprobe.d/cramfs.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/rockysoe/cramfs.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => File['/etc/modprobe.d'],
+  if $enforce_cramfs {
+    file { '/etc/modprobe.d/cramfs.conf':
+      ensure  => file,
+      source  => 'puppet:///modules/rockysoe/cramfs.conf',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => File['/etc/modprobe.d'],
+    }
+
+    exec { 'unload_cramfs':
+      command => 'modprobe -r cramfs',
+      path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+      unless  => 'lsmod | grep cramfs',
+    }
   }
 
-  exec { 'unload_cramfs':
-    command => 'modprobe -r cramfs',
-    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-    unless  => 'lsmod | grep cramfs',
-  }
+  if $enforce_squashfs {
+    file { '/etc/modprobe.d/squashfs.conf':
+      ensure  => file,
+      source  => 'puppet:///modules/rockysoe/squashfs.conf',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => File['/etc/modprobe.d'],
+    }
 
-  file { '/etc/modprobe.d/squashfs.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/rockysoe/squashfs.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => File['/etc/modprobe.d'],
-  }
-
-  exec { 'unload_squashfs':
-    command => 'modprobe -r squashfs',
-    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-    unless  => 'lsmod | grep squashfs',
+    exec { 'unload_squashfs':
+      command => 'modprobe -r squashfs',
+      path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+      unless  => 'lsmod | grep squashfs',
+    }
   }
 }
