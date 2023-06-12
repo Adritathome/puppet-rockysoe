1b71f335c3c2f0ff0ef0f697ca3d9e758c9f1f77
Ibn English <Ibn_robinson@hotmail.com>
2023-06-12 17:02:50 +1000

    removed additional slash

--- manifests/tmpconfig.pp
+++ manifests/tmpconfig.pp
@@ -32,7 +32,7 @@ class rockysoe::tmpconfig {
   # Ensure fstab file is present and uses the template fstab.erb to populate
   file { '/etc/fstab':
     ensure  => file,
-    content => template('/rockysoe/fstab.erb'),
+    content => template('rockysoe/fstab.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
