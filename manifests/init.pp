37b2fb42498ef0e6b1679de86160e5d106485b41
Ibn English <Ibn_robinson@hotmail.com>
2023-06-12 18:04:40 +1000

    set defaults in init

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,12 +8,9 @@ class rockysoe (
   Boolean $enforce_udf = false,
   Boolean $enforce_squashfs = false,
   Boolean $enforce_cramfs = false,
-  $boot_partition,
-  $efi_partition,
+  String  $boot_partition = '',
+  String  $efi_partition  = '',
 ) {
   include rockysoe::unusedfs
-  class { 'rockysoe::tmpconfig':
-    boot_partition => $boot_partition,
-    efi_partition  => $efi_partition,
-  }
+  include rockysoe::tmpconfig
 }
